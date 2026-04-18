d76266bfe40342d0ed6286bad495e2d765526352
Tushar Bhadwaj <tusharrbhardwaj@gmail.com>
2026-04-18 12:35:19 +0200

    Added testing lines to code

--- data/dbConnect.py
+++ data/dbConnect.py
@@ -7,4 +7,11 @@ load_dotenv()
 def connection():  
     db_url = os.getenv("DB_URL")
     conn = psycopg2.connect(db_url)
+    # print("successfull")  # --- line to debug
     return conn
+
+'''
+To test this file
+'''
+
+# connection()
