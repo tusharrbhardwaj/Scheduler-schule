63aea821630119e2224a82775e8eb0dbd3bae009
Tushar Bhadwaj <tusharrbhardwaj@gmail.com>
2026-04-19 02:45:35 +0200

    Created seperate class to fecth result table data

--- data/dbFetch.py
+++ data/dbFetch.py
@@ -1,6 +1,4 @@
-import csv
-
-from dbConnect import connection
+from data import connection
 
 conn = connection()
 cur = conn.cursor()
@@ -18,10 +16,40 @@ class Fetch:
         fetched_data = cur.fetchall()
         return columns, fetched_data
     
-    # def joinfetch(self):
-    #     query = f'''
-            
+    def schedule_fetch(self):
+        query = f'''
+        SELECT 
+            gs.class_id,
+            p.prof_name,
+            gs.room_no,
+            t.day,
+            t.start_time,
+            t.end_time,
+            c.total_students,
+            r.cr_capacity,
+            (r.cr_capacity - c.total_students) AS wasted_seats
+
+        FROM greedy_schedule gs
+
+        JOIN classes c 
+            ON gs.class_id = c.class_id
+
+        JOIN professor p 
+            ON gs.prof_id = p.prof_id
+
+        JOIN classrooms r 
+            ON gs.room_no = r.room_no
+
+        JOIN timeslots t 
+            ON gs.timeslot_id = t.timeslot_id
+
+        ORDER BY 
+            t.day,
+            t.start_time,
+            gs.room_no;
+        '''
         
-    #         '''
-
-
+        cur.execute(query)
+        data = cur.fetchall()
+        
+        return data
